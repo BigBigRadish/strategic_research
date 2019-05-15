d65877b8589b9973be651cf8ae43834f74891cdc
BigBigRadish <luozhukun@163.com>
2019-05-15 21:58:36 +0800

    第二种方式

--- RSRS/tools.py
+++ RSRS/tools.py
@@ -29,10 +29,11 @@ high= alpha + beta*low + epsilon， epsilon ~ N(0,sigma) （1)
 2. 将两列数据按式（1）的模型进行 OLS 线性回归。
 3. 将拟合后的 beta 值作为当日 RSRS斜率指标值。
 另一种则为将斜率标准化，取其标准分作为指标值。 当日标准分指标的计算方式:
-1. 取前 M 日的斜率时间序列。
+1. 取前 M日的斜率时间序列。
 2. 以此样本计算当日斜率的标准分。 
-3. 将计算得到的标准分 z 作为当日 RSRS标准分指标值。 
+3. 将计算得到的标准分 z-score作为当日 RSRS标准分指标值。 
 '''
+#方法一
 def slope_method1(df,n):
     length=df.shape[0]
     slope_param=[]
@@ -46,20 +47,50 @@ def slope_method1(df,n):
         slope_param.append(param[1])
     df1['day_slope']=slope_param
     return df1
+#方法二：
+'''
+）总体数据的均值（μ）
+
+     在上面的例子中，总体可以是整个班级的平均分，也可以是全市、全国的平均分。
+
+2）总体数据的标准差（σ）
+
+     这个总体要与1）中的总体在同一个量级。
+
+3）个体的观测值（x）
+'''
+def slop_method2(df,n,m):
+    df1=slope_method1(df,n)
+    length=df.shape[0]
+    z_scores=[]
+    df1=df1.reset_index()
+    df2=df1[m:]
+    for j in range(0,length-m):
+        m_day_df=df1.iloc[j:j+m]
+        print()
+        slope=m_day_df['day_slope'].values
+        avg=np.mean(slope)
+        std=np.std(slope)
+        print(avg,std)
+    
+    
+    
 def ols_regression(x,y,eps):
     X=sm.add_constant(x) 
     model = sm.OLS(y-eps,X)
     results = model.fit() 
     return(results.params) 
 if __name__=='__main__':
-    file_path='./data/000016.SH.mat'
+    file_path='./data/000905.SH.mat'
     df=mat_to_df(file_path)
-    df1=slope_method1(df,18)
+    df.date=df.date.apply(lambda i : str(int(i)))
+#     df1=slope_method1(df,18)
 #     plt.plot(df1['lowest_price_today'].values,df1['highest_price_today'])
 #     plt.show()
-    df1.date=df1.date.apply(lambda i : str(int(i)))
-    print(df1.date)
-    df1.to_csv('./data/000016.SH_1.csv')
+#     df1.date=df1.date.apply(lambda i : str(int(i)))
+#     print(df1.date)
+#     df1.reset_index().to_csv('./data/000905.SH_1.csv',)
+    df1=slop_method2(df,18,5)
     
     
     
